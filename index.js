c532c8054def3e9254af6f46d0e3dad423937fd1
prrrcl <adrianporcelnavarro@hotmail.com>
2019-07-16 14:23:53 +0200

    Done

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor () {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add (item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get (pos) {
+    return this.items[pos - 1];
+  }
+
+  max () {
+    if (this.length > 0) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  };
+
+  min () {
+    if (this.length > 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
+
+  average () {
+    if (this.length > 0) {
+      return this.items.reduce((a, b) => a + b, 0) / this.items.length;
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
+
+  sum () {
+    return this.items.reduce((a, b) => a + b, 0);
+  }
 };
+const instance = new SortedList();
+instance.add('pepito');
+instance.get(0);
 
 module.exports = SortedList;
