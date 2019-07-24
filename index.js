69065390a3bf06076d49151019ae219c552d91d5
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2019-07-24 17:55:08 +0200

    Update index.js
    wip

--- index.js
+++ index.js
@@ -5,23 +5,29 @@ class SortedList {
   }
   add(item) {
     this.items.push(item);
+    this.items.sort();
     this.length = this.items.length;
   }
 
   get(pos) {
-    if (this.items.length - 1 < pos) {
+    if (!this.items[pos - 1]) {
       throw new Error("OutOfBounds");
-    }
-   else return this.items[pos];
+    } else return this.items[pos - 1];
   }
   max() {
     if (this.items.length === 0) {
       throw new Error("EmptySortedList");
     } else {
-      return this.items.length;
+      return this.items[this.length - 1];
     }
   }
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
   average() {}
   sum() {}
 }
