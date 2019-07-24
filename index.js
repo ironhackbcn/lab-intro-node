294508ce57f8ed3b047561f92cac341dd7174187
Juan Vicente Vazquez G <juanvicentevazquezg@gmail.com>
2019-07-24 06:36:20 +0200

    Second Initial Commit

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
+  constructor() {
+    this.item = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.item.push(item);
+    this.lenght=this.item.length;
+  }
   get(pos) {}
-  max() {}
+  max() {
+    if (this.item.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.item.length;
+    }
+  }
   min() {}
   average() {}
   sum() {}
-};
-
+}
+const sortedList = new SortedList 
 module.exports = SortedList;
+module.exports = sortedList;
