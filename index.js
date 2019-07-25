fd34bc2ae543d44a8fc9ec92e0281f7de9fb606e
Victor Camarena Ballester <victor.camarena.ballester@gmail.com>
2019-07-25 16:27:23 +0200

    done

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length += 1;
+    this.items.sort((value1, value2) => value1 - value2);
+  }
+  get(pos) {
+    try {
+      return this.items[pos - 1];
+    } catch (error) {
+      throw new Error("OutOfBounds");
+    }
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+  average() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return (
+        this.items.reduce((valAnterior, valActual) => valAnterior + valActual) /
+        this.length
+      );
+    }
+  }
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce(
+        (valAnterior, valActual) => valAnterior + valActual
+      );
+    }
+  }
+}
 
 module.exports = SortedList;
