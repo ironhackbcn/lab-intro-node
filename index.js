1f875c64fd0e3c1daebe433d6a2068d989bd2a83
Marc Valles Casares <13a1911@gmail.com>
2019-07-26 06:53:11 +0200

    empezado, algunos test hechos

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+  }
+  get(pos) {
+    return this.items[pos - 1];
+  }
   max() {}
   min() {}
   average() {}
   sum() {}
-};
+}
 
 module.exports = SortedList;
+
+// var sl = new SortedList();
+
+// var foo = 10;
+// for (let i = 0; i < 200; i++) {
+//   sl.add(foo * i);
+//   sl.get(i + 1);
+// }
