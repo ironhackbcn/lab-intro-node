dfed50599717178f91f0c659cc1039786ffe2310
Xavi Gallardo <xavigallardo@gmail.com>
2019-07-26 19:16:31 +0200

    done

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    try {
+      return this.items[pos - 1];
+    } catch (error) {
+      throw new Error("OutOfList");
+    }
+  }
+  max() {
+    // try {
+    //   return this.items[this.length - 1]; -> Es undefined , NO ERROR !!!
+    // } catch (error) {
+    //   throw new Error("EmptySortedList");
+    // }
+
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+  sum() {
+    return this.items.reduce((accum, actualValue) => accum + actualValue, 0);
+  }
+}
 
 module.exports = SortedList;
