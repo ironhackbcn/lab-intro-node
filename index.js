f8024127629dee1f415403697f9294c9d7242128
Nomeolvides <info@nomeolvides.cat>
2019-07-29 23:18:20 +0200

    done

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
+  add(x) {
+    this.items.push(x);
+    this.length++;
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+  }
+  get(i) {
+    i = i - 1;
+    if (this.items[i] != null) {
+      return this.items[i];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
+  max() {
+    if (this.items.length > 0) {
+      return Math.max(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  min() {
+    if (this.items.length > 0) {
+      return Math.min(...this.items);
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  average() {
+    if (this.items.length > 0) {
+      return this.sum() / this.items.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
+  sum() {
+    if (this.items.length > 0) {
+      return this.items.reduce((acc, value) => acc + value);
+    } else {
+      return 0;
+    }
+  }
+}
 
 module.exports = SortedList;
