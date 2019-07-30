31ab020b4e3c9af577c9e79b373791186bd2a503
Elena Piaggio <helenamercury@gmail.com>
2019-07-30 18:44:36 +0200

    partial

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
-};
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    return this.items[pos + 1];
+  }
+
+  max() {
+    if(this.length < 1){
+      throw new Error("EmptySortedList");
+    } else{
+      let max = 0;
+      this.items.forEach( element =>{
+        if(element > max){
+          max = element;
+        }
+      });
+    }
+    return this.max;
+  }
+
+  min() {
+    if(this.length < 1){
+      throw new Error("EmptySortedList");
+    } else{
+      let min = this.items[0];
+      this.items.forEach( element =>{
+        if(element < min){
+          min = element;
+        }
+      });
+    }
+    return this.min;
+  }
+
+  average() {
+    let theAverage = 0;
+    if(this.length < 1){
+      throw new Error("EmptySortedList");
+    } else{
+      this.items.forEach( element =>{
+        theAverage+= element;
+      });
+    }
+    return theAverage/this.length;
+  }
+
+  sum() {
+    let theSum = 0;
+    if(this.length < 1){
+      throw new Error("EmptySortedList");
+    } else{
+      this.items.forEach( element =>{
+        theSum+= element;
+      });
+    }
+    return theSum;
+  }
+}
 
 module.exports = SortedList;
