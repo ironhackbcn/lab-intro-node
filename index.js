c53648e082fc961177e681642ea162a093de2a3b
Olga Dokukova <dokukova@gmail.com>
2019-08-01 00:43:51 +0200

    done

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((i, j) => i - j);
+  }
+  get(pos) {
+    try {
+      return this.items[pos - 1]
+    } catch (error) {
+      throw new Error("NotInList");
+    }
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
+  sum() {
+    return this.items.reduce((sum, item) => sum + item, 0);
+  }
 };
 
 module.exports = SortedList;
