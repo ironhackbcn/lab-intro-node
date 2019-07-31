af03f541166200fba0478d46bccb908263b0365e
Elena Piaggio <helenamercury@gmail.com>
2019-07-31 23:46:24 +0200

    done

--- index.js
+++ index.js
@@ -6,60 +6,67 @@ class SortedList {
 
   add(item) {
     this.items.push(item);
-    this.length = this.items.length;
+    this.length++;
+    this.items.sort(
+      (a, b) => a - b
+    );
   }
 
-  get(pos) {
-    return this.items[pos + 1];
+  get(i) {
+    if (this.items[i - 1] === undefined) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[i - 1];
+    }
   }
 
   max() {
-    if(this.length < 1){
+    let max = 0;
+    if (this.length === 0) {
       throw new Error("EmptySortedList");
-    } else{
-      let max = 0;
-      this.items.forEach( element =>{
-        if(element > max){
+    } else {
+      this.items.forEach(element => {
+        if (element > max) {
           max = element;
         }
       });
     }
-    return this.max;
+    return max;
   }
 
   min() {
-    if(this.length < 1){
+    let min = this.items[0];
+    if (this.length === 0) {
       throw new Error("EmptySortedList");
-    } else{
-      let min = this.items[0];
-      this.items.forEach( element =>{
-        if(element < min){
+    } else {
+      this.items.forEach(element => {
+        if (element < min) {
           min = element;
         }
       });
     }
-    return this.min;
+    return min;
   }
 
   average() {
     let theAverage = 0;
-    if(this.length < 1){
+    if (this.length < 1) {
       throw new Error("EmptySortedList");
-    } else{
-      this.items.forEach( element =>{
-        theAverage+= element;
+    } else {
+      this.items.forEach(element => {
+        theAverage += element;
       });
     }
-    return theAverage/this.length;
+    return theAverage / this.length;
   }
 
   sum() {
     let theSum = 0;
-    if(this.length < 1){
+    if (this.length === undefined) {
       throw new Error("EmptySortedList");
-    } else{
-      this.items.forEach( element =>{
-        theSum+= element;
+    } else {
+      this.items.forEach(element => {
+        theSum += element;
       });
     }
     return theSum;
