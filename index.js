106f9c58adf5164933db02bfdd3db7b110a6ccc8
Àlex <alexmanye@gmail.com>
2020-03-31 20:44:49 +0200

    refactoring tests done

--- index.js
+++ index.js
@@ -1,61 +1,75 @@
-//Hay errores, para revisar más tarde
+'use strict'
+
 class SortedList {
-  constructor() {
+  constructor(){
     this.items = [];
     this.length = 0;
   }
+  //test 2, 3 & 4
   add(item) {
-    this.items.push(item)
-    this.items.sort(function (a, b) {
-      return a - b;
-    });
-    this.length = this.items.length;
-
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length += 1;
   }
-  get(pos) {
-    return this.items[pos - 1];
+  //test 5 & 6
+  get(position){
+    if (!this.items[position - 1]){
+      throw new Error('OutOfBounds');
+    } 
+    return this.items[position - 1];
   }
 
-  max() {
-    try {
-      if (this.length === 0) {
-        throw 'EmptySortedList'
-      } else {
-        return Math.max[...this.items] //no me lo pilla
+  //test 7 & 8
+
+  max(){
+    try{
+      if (this.items.length === 0){
+      throw `EmptySortedList`;
+      } else{
+        return Math.max(...this.items);
+        }
+    } catch(error){
+    throw new Error(error);
+    }
+  };
+
+  //test 9 y 10
+  min(){
+    try{
+      if (this.items.length === 0){
+      throw `EmptySortedList`;
+      } else{
+        return Math.min(...this.items);
       }
-    } catch (error) {
-      throw new Error(error)
+    } catch(error){
+    throw new Error(error);
+  }
+}
+
+ //test 11 y 12
+
+ sum(){
+  if (this.items.length === 0){
+    return 0;
+  } else{
+    return this.items.reduce((acum, current) => {
+    return acum += current});
     }
   }
 
-  min() {
-    try {
-      if (this.length === 0) {
-        throw 'EmptySortedList'
-      } else {
-        return this.items[0]
-      }
-    } catch (error) {
-      throw new Error(error)
-    }
-  }
+ //test 13 y 14
 
-  average() {
-    if (this.items.length === 0) {
-      throw new Error("EmptySortedList");
-
-    } else {
-      return this.sum() / this.items.length;
-    }
+average(){
+  try{
+  if (this.items.length === 0){
+    throw 'EmptySortedList';
+  } else {
+    return (this.sum()/this.items.length);
   }
-
-  sum() {
-    if (this.items.length === 0) {
-      return 0
-    } else {
-      return this.items.reduce((a, b) => {
-        return a + b
-      })
-    }
+  } catch(error){
+    throw new Error(error);
   }
-  module.exports = SortedList;
+}
+}
+
+module.exports = SortedList;
